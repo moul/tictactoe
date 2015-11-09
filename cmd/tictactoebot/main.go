067d325eab5c515ce447dfb5fa1733cf01209e0c
Manfred Touron <m@42.am>
2015-11-09 23:24:13 +0100

    Initial version

---
+++ cmd/tictactoebot/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/moul/tictactoe"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		ttt := tictactoe.NewTicTacToe()
+		ttt.Set(0, 0, c.Query("0-0"))
+		ttt.Set(0, 1, c.Query("0-1"))
+		ttt.Set(0, 2, c.Query("0-2"))
+		ttt.Set(1, 0, c.Query("1-0"))
+		ttt.Set(1, 1, c.Query("1-1"))
+		ttt.Set(1, 2, c.Query("1-2"))
+		ttt.Set(2, 0, c.Query("2-0"))
+		ttt.Set(2, 1, c.Query("2-1"))
+		ttt.Set(2, 2, c.Query("2-2"))
+		ttt.SetPlayer(c.Query("you"))
+
+		fmt.Println(ttt.ShowMap())
+		fmt.Printf("Winner: %q\n", ttt.Winner())
+
+		if c.Query("show-map") == "1" {
+			c.String(200, ttt.ShowMap())
+			return
+		}
+
+		next, err := ttt.Next()
+		if err != nil {
+			c.String(404, fmt.Sprintf("Error: %v", err))
+		} else {
+			c.String(200, fmt.Sprintf("%d-%d", next.Y, next.X))
+		}
+	})
+	r.Run(":8080")
+}
---
+++ tictactoe.go
@@ -0,0 +1,202 @@
+package tictactoe
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/moul/ssh2docker/vendor/github.com/Sirupsen/logrus"
+)
+
+type TicTacToe struct {
+	Map    map[int]map[int]string
+	Player string
+	Size   int
+}
+
+type Movement struct {
+	X     int
+	Y     int
+	Score int
+}
+
+func NewTicTacToe() TicTacToe {
+	return NewTicTacToeWithSize(3)
+}
+
+func NewTicTacToeWithSize(size int) TicTacToe {
+	ttt := TicTacToe{
+		Size: size,
+		Map:  make(map[int]map[int]string, size),
+	}
+	for i := 0; i < size; i++ {
+		ttt.Map[i] = make(map[int]string, size)
+	}
+	return ttt
+}
+
+func (t *TicTacToe) SetPlayer(symbol string) {
+	t.Player = symbol
+}
+
+func (t *TicTacToe) Set(y, x int, symbol string) {
+	t.Map[y][x] = symbol
+}
+
+func (t *TicTacToe) ShowMap() string {
+	lines := []string{}
+	for i := 0; i < t.Size; i++ {
+		line := []string{}
+		for j := 0; j < t.Size; j++ {
+			if t.Map[i][j] == "" {
+				line = append(line, ".")
+			} else {
+				line = append(line, t.Map[i][j])
+			}
+		}
+		lines = append(lines, strings.Join(line, " "))
+	}
+	return strings.Join(lines, "\n")
+}
+
+func (t *TicTacToe) Winner() string {
+	// horizontal
+	for y := 0; y < t.Size; y++ {
+		symbol := t.Map[y][0]
+		ok := true
+		for x := 1; x < t.Size; x++ {
+			if symbol != t.Map[y][x] {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			return symbol
+		}
+	}
+
+	// vertical
+	for x := 0; x < t.Size; x++ {
+		symbol := t.Map[0][x]
+		ok := true
+		for y := 1; y < t.Size; y++ {
+			if symbol != t.Map[y][x] {
+				ok = false
+				break
+			}
+		}
+		if ok {
+			return symbol
+		}
+	}
+
+	// diagonal
+	symbol := t.Map[0][0]
+	ok := true
+	for i := 1; i < t.Size; i++ {
+		if t.Map[i][i] != symbol {
+			ok = false
+			break
+		}
+	}
+	if ok {
+		return symbol
+	}
+	symbol = t.Map[t.Size-1][0]
+	ok = true
+	for i := 1; i < t.Size; i++ {
+		if t.Map[t.Size-1-i][i] != symbol {
+			ok = false
+			break
+		}
+	}
+	if ok {
+		return symbol
+	}
+
+	return ""
+}
+
+func (t *TicTacToe) AvailableMoves() []Movement {
+	moves := []Movement{}
+	for y := 0; y < t.Size; y++ {
+		for x := 0; x < t.Size; x++ {
+			if t.Map[y][x] == "" {
+				moves = append(moves, Movement{Y: y, X: x})
+			}
+		}
+	}
+	return moves
+}
+
+func (t *TicTacToe) ScoreMoves(currentPlayer string, deepness int) []Movement {
+	// if map is finished, return nil
+	if t.Winner() != "" {
+		return nil
+	}
+
+	if deepness > 10 {
+		logrus.Errorf("deepness > 10")
+		return nil
+	}
+
+	moves := t.AvailableMoves()
+
+	for idx, move := range moves {
+		t.Set(move.Y, move.X, currentPlayer)
+		switch t.Winner() {
+		case t.Player:
+			moves[idx].Score += 10 * (9 - deepness)
+		case t.Opponent():
+			moves[idx].Score -= 10 * (9 - deepness)
+		default:
+			for _, subMove := range t.ScoreMoves(t.NextPlayer(currentPlayer), deepness+1) {
+				moves[idx].Score += subMove.Score
+			}
+		}
+		t.Set(move.Y, move.X, "")
+	}
+
+	return moves
+}
+
+func (t *TicTacToe) NextPlayer(currentPlayer string) string {
+	switch currentPlayer {
+	case "X":
+		return "O"
+	case "O":
+		return "X"
+	}
+	return ""
+}
+
+func (t *TicTacToe) Opponent() string {
+	return t.NextPlayer(t.Player)
+}
+
+func (t *TicTacToe) Next() (*Movement, error) {
+	if t.Winner() != "" {
+		return nil, fmt.Errorf("game is already finished")
+	}
+
+	if len(t.AvailableMoves()) == t.Size*t.Size {
+		return &Movement{Y: t.Size / 2, X: t.Size / 2}, nil
+	}
+
+	moves := t.ScoreMoves(t.Player, 1)
+
+	if len(moves) == 0 {
+		return nil, fmt.Errorf("no such move")
+	}
+
+	maxIdx := 0
+	maxScore := moves[0].Score
+
+	for idx, move := range moves {
+		if move.Score > maxScore {
+			maxScore = move.Score
+			maxIdx = idx
+		}
+	}
+	move := moves[maxIdx]
+	return &move, nil
+}
