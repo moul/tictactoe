342cb9bd3a5de780459ded7ae949fb123fb97c80
Manfred Touron <m@42.am>
2016-05-25 09:52:47 +0200

    Refactored bot as a struct

--- cmd/tictactoe-bolosseum/main.go
+++ cmd/tictactoe-bolosseum/main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/moul/bolosseum/bots"
 	"github.com/moul/tictactoe"
 )
 
@@ -15,50 +16,73 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
-type QuestionMessage struct {
-	GameID      string
-	Action      string
-	Game        string
-	Players     int
-	Board       map[string]string
-	You         string
-	PlayerIndex int
+func NewTictactoeBot() *TictactoeBot {
+	return &TictactoeBot{}
+}
+
+type TictactoeBot struct{}
+
+func (b *TictactoeBot) Init(message bots.QuestionMessage) *bots.ReplyMessage {
+	// FIXME: init ttt here
+	return &bots.ReplyMessage{
+		Name: "moul-tictactoe",
+	}
+}
+
+func (b *TictactoeBot) PlayTurn(question bots.QuestionMessage) *bots.ReplyMessage {
+	ttt := tictactoe.NewTicTacToe()
+
+	board := question.Board.(map[string]interface{})
+
+	ttt.Set(0, 0, board["0-0"].(string))
+	ttt.Set(0, 1, board["0-1"].(string))
+	ttt.Set(0, 2, board["0-2"].(string))
+	ttt.Set(1, 0, board["1-0"].(string))
+	ttt.Set(1, 1, board["1-1"].(string))
+	ttt.Set(1, 2, board["1-2"].(string))
+	ttt.Set(2, 0, board["2-0"].(string))
+	ttt.Set(2, 1, board["2-1"].(string))
+	ttt.Set(2, 2, board["2-2"].(string))
+	ttt.SetPlayer(question.You.(string))
+
+	logrus.Debugf("map: %q", ttt.ShowMap())
+
+	next, err := ttt.Next()
+	if err != nil {
+		logrus.Fatalf("Error while getting the next piece: %v", err)
+	}
+
+	return &bots.ReplyMessage{
+		Play: fmt.Sprintf("%d-%d", next.Y, next.X),
+	}
 }
 
 func main() {
 	logrus.Warnf("%s << %v", os.Args[0], os.Args[1])
 
-	var question QuestionMessage
+	var question bots.QuestionMessage
 	if err := json.Unmarshal([]byte(os.Args[1]), &question); err != nil {
 		logrus.Fatalf("%s XX err: %v", err)
 	}
 
+	bot := NewTictactoeBot()
+
+	reply := &bots.ReplyMessage{}
+
 	switch question.Action {
 	case "init":
-		logrus.Warnf("init: %v", question)
-		fmt.Println("{\"name\":\"moul-tictactoe\"}")
+		reply = bot.Init(question)
 	case "play-turn":
-		ttt := tictactoe.NewTicTacToe()
-		ttt.Set(0, 0, question.Board["0-0"])
-		ttt.Set(0, 1, question.Board["0-1"])
-		ttt.Set(0, 2, question.Board["0-2"])
-		ttt.Set(1, 0, question.Board["1-0"])
-		ttt.Set(1, 1, question.Board["1-1"])
-		ttt.Set(1, 2, question.Board["1-2"])
-		ttt.Set(2, 0, question.Board["2-0"])
-		ttt.Set(2, 1, question.Board["2-1"])
-		ttt.Set(2, 2, question.Board["2-2"])
-		ttt.SetPlayer(question.You)
-
-		logrus.Debugf("map: %q", ttt.ShowMap())
-
-		next, err := ttt.Next()
-		if err != nil {
-			logrus.Fatalf("Error while getting the next piece: %v", err)
-		}
-
-		fmt.Printf("{\"play\":\"%d-%d\"}", next.Y, next.X)
+		reply = bot.PlayTurn(question)
 	default:
+		// FIXME: reply message error
 		logrus.Fatalf("Unknown action: %q", question.Action)
 	}
+
+	jsonString, err := json.Marshal(reply)
+	if err != nil {
+		logrus.Fatalf("Failed to marshal json: %v", err)
+	}
+
+	fmt.Println(string(jsonString))
 }
