f3b230900c77939ca63893ab555b86cc729786fd
Manfred Touron <m@42.am>
2016-05-28 20:28:32 +0200

    Support POST

--- cmd/tictactoe-bolosseum/main.go
+++ cmd/tictactoe-bolosseum/main.go
@@ -15,6 +15,7 @@ import (
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
+	gin.DisableBindValidation()
 }
 
 func main() {
@@ -26,6 +27,31 @@ func main() {
 		logrus.Warnf("- %s some-json  # cli mode", os.Args[0])
 
 		r := gin.Default()
+
+		r.POST("/", func(c *gin.Context) {
+			var question bots.QuestionMessage
+			if err := c.BindJSON(&question); err != nil {
+				fmt.Println(err)
+				c.JSON(404, fmt.Errorf("Invalid POST data: %v", err))
+				return
+			}
+
+			bot := tictactoebot.NewTictactoeBot()
+			reply := &bots.ReplyMessage{}
+			switch question.Action {
+			case "init":
+				reply = bot.Init(question)
+			case "play-turn":
+				reply = bot.PlayTurn(question)
+			default:
+				// FIXME: reply message error
+				c.JSON(500, gin.H{"Error": fmt.Errorf("Unknown action: %q", question.Action)})
+				return
+			}
+
+			c.JSON(200, reply)
+		})
+
 		r.GET("/", func(c *gin.Context) {
 			if message := c.Query("message"); message != "" {
 				bot := tictactoebot.NewTictactoeBot()
